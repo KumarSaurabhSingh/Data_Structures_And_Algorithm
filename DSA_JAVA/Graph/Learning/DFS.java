71fded408b10b47ab7a68dbfa00d213b48bbf35a
KumarSaurabhSingh <iamkuwarsaurabhkumarsingh@gmail.com>
2025-03-14 01:11:14 +0530

    New Code

--- DSA_JAVA/Graph/Learning/DFS.java
+++ DSA_JAVA/Graph/Learning/DFS.java
@@ -1,5 +1,11 @@
 import java.util.ArrayList;
 
+//Time Complexity: For an undirected graph, O(N) + O(2E), For a directed graph, O(N) + O(E), Because for every node we are calling the recursive function once, the time taken is O(N) and 2E is for total degrees as we traverse for all adjacent nodes.
+//Space Complexity: O(3N) ~ O(N), Space for dfs stack space, visited array and an adjacency list.
+
+
+
+
 public class DFS {
     
     public static void dfs(int node, boolean[] visit, ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> lst){
